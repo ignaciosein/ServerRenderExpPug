24ffa07a289842b892b4ecdb7ae60341f442d9af
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-06-02 16:16:40 +0200

    Cambio definitivo

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,6 @@ router.get('/', pages.home)
 router.get('/film/:title', pages.getFilm)
 router.post('/film', pages.postFilm)
  
-/* router.get('*', pages.home) //ruta por defecto */
+/* router.get('*', pages.home) //ruta  efecto */
 
 module.exports = router;
