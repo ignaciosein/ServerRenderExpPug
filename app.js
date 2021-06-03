cc4dc6beb8dfc63d4955c923d0125f3f277c0e77
Diego Bertelloni <diegobertelloni@Diegos-MacBook-Air.local>
2021-06-03 17:01:12 +0200

    cambios app y package

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const app = express();
 const router = require("./controllers/index");
 const routerApi = require('./controllers/routerApi');
 
-
 app.use("/public", express.static("public"));
- 
-const port = 3000;
+
+const port = 3000 || process.env.PORT;
 
 // motor de vistas
 app.set("view engine", "pug");
 app.set("views", "./views");
- 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -20,5 +19,4 @@ app.use('/api', routerApi);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
- 
 });
