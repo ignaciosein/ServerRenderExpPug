55ce39868b429f6d2803ef608ae5d8415c769bd0
Diego Bertelloni <diegobertelloni@Diegos-MacBook-Air.local>
2021-06-03 16:46:59 +0200

    cambio puerto appJs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const routerApi = require('./controllers/routerApi');
 
 app.use("/public", express.static("public"));
  
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 // motor de vistas
 app.set("view engine", "pug");
